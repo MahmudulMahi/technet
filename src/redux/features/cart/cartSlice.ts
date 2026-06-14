3bd66cce19dfdd80fba85afe5f22fa25ae81ca3b
mahmudul <mahmudul35-218@diu.edu.bd>
2026-06-14 16:17:37 +0600

    remove item

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -29,21 +29,7 @@ const cartSlice = createSlice({
 
       state.total += action.payload.price;
     },
-    removeOne: (state, action: PayloadAction<IProduct>) => {
-      const existing = state.products.find(
-        (product) => product._id === action.payload._id
-      );
 
-      if (existing && existing.quantity! > 1) {
-        existing.quantity = existing.quantity! - 1;
-      } else {
-        state.products = state.products.filter(
-          (product) => product._id !== action.payload._id
-        );
-      }
-
-      state.total -= action.payload.price;
-    },
     removeFromCart: (state, action: PayloadAction<IProduct>) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload._id
