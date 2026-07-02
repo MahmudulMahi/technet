36040480ae9e9b07a413a82e4a6b5f4d5cb3509f
mahmudul <mahmudul35-218@diu.edu.bd>
2026-07-02 16:03:56 +0600

    sss

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -25,12 +25,7 @@ export default function Login() {
                 'url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80)',
             }}
           />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <img className="h-8" src={logo} alt="" />
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2"></blockquote>
-          </div>
+
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
