80259a53b58bf2626b0192b241c318ec72d4351f
mahmudul <mahmudul35-218@diu.edu.bd>
2026-07-08 16:13:36 +0600

    edit login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -33,34 +33,7 @@ export default function Login() {
           </div>
         </div>
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Login to your account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below
-              </p>
-            </div>
-            <LoginForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{' '}
-              <Link
-                to="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{' '}
-              and{' '}
-              <Link
-                to="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
-          </div>
+
         </div>
       </div>
     </>
