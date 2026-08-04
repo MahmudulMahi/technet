cc6dd36c2605aab140544f33ff7e01206c988a5c
mahmudul <mahmudul35-218@diu.edu.bd>
2026-08-04 14:50:19 +0600

    api slice

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -58,16 +58,7 @@ export function LoginForm({ className, ...props }: UserAuthFormProps) {
           <Button>Login with email</Button>
         </div>
       </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
+
       <Button
         variant="outline"
         type="button"
